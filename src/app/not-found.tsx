03484c81cd2c7fec90ce01da4e935cca3354e0cd
esraa khaled <esraakhaledharfoush@gmail.com>
2025-10-01 23:02:16 +0200

    oops

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,20 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function Notfound() {
-  return (
-    <div>
-      Not-found
-    </div>
-  )
+  return<>
+  <div>
+   <div className=" h-screen flex items-center justify-center">
+  <Image
+    src="/images/404 Error Page not Found with people connecting a plug-bro (1).svg"
+    alt="error"
+   width={500}
+   height={500}
+    className="object-cover"
+  />
+</div>
+
+
+  </div>
+  </>
 }
